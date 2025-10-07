1fdf72d1a9b89f834ff951f721483a7a459a0efd
Florent L-L <flothival@gmail.com>
2025-10-07 22:59:07 +0200

    encode implement

--- Main.java
+++ Main.java
@@ -2,23 +2,47 @@ import java.util.Scanner;
 
 public class Main {
 
-    @TODO
     private static String encode(String s) {
+        String meuh = "";
+        for(int i = 0; i < s.length(); i++) {
+            char lettre = s.charAt(i);
+            String binary = Integer.toBinaryString(lettre);
 
+            while(binary.length() < 8) {
+                binary = "0" + binary;
+            }
+
+            char[] tab = binary.toCharArray();
+
+            meuh += (tab[0] == '1') ? "M" : "h";
+            meuh += (tab[1] == '1') ? "E" : "e";
+            meuh += (tab[2] == '1') ? "U" : "u";
+            meuh += (tab[3] == '1') ? "H" : "h";
+            meuh += " ";
+            meuh += (tab[4] == '1') ? "M" : "h";
+            meuh += (tab[5] == '1') ? "E" : "e";
+            meuh += (tab[6] == '1') ? "U" : "u";
+            meuh += (tab[7] == '1') ? "H" : "h";
+            meuh += " ";
+
+        }
+
+        return meuh;
     }
-
+/* 
     @TODO
     private static String decode(String key) {
 
     }
-
+*/
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("*********** Welcome to MEUH encoding ***********\n ********************************************\n\n");
+        System.out.print("************************************************\n*********** Welcome to MEUH encoding ***********\n************************************************\n\n");
         System.out.println("1. encode");
         System.out.println("2. decode");
         System.out.print("select a function : ");
         int choice = scanner.nextInt();
+        scanner.nextLine();
         String s = "";
         switch (choice) {
             case 1:
@@ -29,7 +53,7 @@ public class Main {
             case 2:
                 System.out.println("Please enter the key : ");
                 s = scanner.nextLine();
-                System.out.println(decode(s));
+                //System.out.println(decode(s));
 
                 break;
         
