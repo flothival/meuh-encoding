3a04e2767652936c6e02fabfd10d2a676f6315b3
Florent L-L <flothival@gmail.com>
2025-10-13 11:02:15 +0200

    modif + ajout de la fonction decode

--- Main.java
+++ Main.java
@@ -4,22 +4,22 @@ public class Main {
 
     private static String encode(String s) {
         String meuh = "";
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char lettre = s.charAt(i);
             String binary = Integer.toBinaryString(lettre);
 
-            while(binary.length() < 8) {
+            while (binary.length() < 8) {
                 binary = "0" + binary;
             }
 
             char[] tab = binary.toCharArray();
 
-            meuh += (tab[0] == '1') ? "M" : "h";
+            meuh += (tab[0] == '1') ? "M" : "m";
             meuh += (tab[1] == '1') ? "E" : "e";
             meuh += (tab[2] == '1') ? "U" : "u";
             meuh += (tab[3] == '1') ? "H" : "h";
             meuh += " ";
-            meuh += (tab[4] == '1') ? "M" : "h";
+            meuh += (tab[4] == '1') ? "M" : "m";
             meuh += (tab[5] == '1') ? "E" : "e";
             meuh += (tab[6] == '1') ? "U" : "u";
             meuh += (tab[7] == '1') ? "H" : "h";
@@ -29,15 +29,30 @@ public class Main {
 
         return meuh;
     }
-/* 
-    @TODO
-    private static String decode(String key) {
 
+    private static String decode(String key) {
+        String binary = "";
+        String result = "";
+        for (int i = 0; i < key.length(); i++) {
+            char c = key.charAt(i);
+            if (c == ' ')
+                continue;
+
+            binary += Character.isUpperCase(c) ? "1" : "0";
+
+            if (binary.length() == 8) {
+                int valeur = Integer.parseInt(binary, 2);
+                result += (char) valeur;
+                binary = "";
+            }
+        }
+        return result;
     }
-*/
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("************************************************\n*********** Welcome to MEUH encoding ***********\n************************************************\n\n");
+        System.out.print(
+                "************************************************\n*********** Welcome to MEUH encoding ***********\n************************************************\n\n");
         System.out.println("1. encode");
         System.out.println("2. decode");
         System.out.print("select a function : ");
@@ -53,14 +68,14 @@ public class Main {
             case 2:
                 System.out.println("Please enter the key : ");
                 s = scanner.nextLine();
-                //System.out.println(decode(s));
+                System.out.println(decode(s));
 
                 break;
-        
+
             default:
                 System.out.println("bad choice");
                 break;
         }
-        
+
     }
 }
